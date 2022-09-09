6668fa2e2b9d5b37953579d3f7d6759ac8997461
Gokulakrishna05 <gokulakrishna76@gmail.com>
2022-09-08 18:23:17 -0700

    Completed

--- Arasan/Models/Master/ItemName.cs
+++ Arasan/Models/Master/ItemName.cs
@@ -2,21 +2,6 @@
 {
     public class ItemName
     {
-        public int Id { get; set; }
-        public string ItemCategory { get; set; }
-        public string ItemGroup { get; set; }
-        public int ItemCode { get; set; }
-        public string ItemNames { get; set; }
-        public int ReorderQuantity { get; set; }
-        public int ReorderLevel { get; set; }
-        public int MaximumStockLevel { get; set; }
-        public int MinimumStockLevel { get; set; }
-        public string ConversionFactor { get; set; }
-        public string UOM  { get; set; }
-
-
-
-
-
+       
     }
 }
