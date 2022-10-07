cad58d04168854e698fa0f505b1cc029e27b86a5
Gokulakrishna05 <gokulakrishna76@gmail.com>
2022-10-06 17:56:56 -0700

    Complete

--- Arasan/Interface/Master/IItemNameService.cs
+++ Arasan/Interface/Master/IItemNameService.cs
@@ -1,9 +1,14 @@
 ﻿using Arasan.Models;
 using System.Collections.Generic;
 using System.Collections;
+using System.Data;
+
 namespace Arasan.Interface.Master
 {
     public interface IItemNameService
     {
+        DataTable GetItemGroup();
+        DataTable GetItemCategory();
+        DataTable GetItemSubGroup();
     }
 }
--- Arasan/Models/Master/ItemName.cs
+++ Arasan/Models/Master/ItemName.cs
@@ -1,10 +1,27 @@
 ﻿
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace Arasan.Models
 {
     public class ItemName
     {
-       
+        public ItemName()
+        {
+            this.IgLst = new List<SelectListItem>();
+            this.Iclst = new List<SelectListItem>();
+            this.Isglst = new List<SelectListItem>();
+            
+        }
+        public List<SelectListItem> IgLst;
+        public string ItemGroup { get; set; }
+        public string ItemCategory { get; set; }
+
+        public List<SelectListItem> Iclst;
+        public string ItemSubGroup { get; set; }
+
+        public List<SelectListItem> Isglst;
+
+
     }
-    
+
 }
