6d07c5dddbc9492d9992582df6f9807173f2d23e
kesavamoorthi007 <kesavanmoorthi81@gmail.com>
2022-10-12 18:13:11 +0530

    Complete

--- Arasan/Controllers/Master/BranchController.cs
+++ Arasan/Controllers/Master/BranchController.cs
@@ -1,28 +1,91 @@
-﻿
-
-
-
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System.Data;
+using Arasan.Interface;
 using Arasan.Interface.Master;
 using Arasan.Models;
+using Arasan.Services.Master;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace Arasan.Controllers.Master
 {
     public class BranchController : Controller
     {
-        IBranchService branchService;
-            public BranchController(IBranchService _branchService)
-            {
-                branchService = _branchService;
-            }
-            public IActionResult Branch()
+        IBranchService BranchService;
+        public BranchController(IBranchService _BranchService)
         {
-            return View();
+            BranchService = _BranchService;
+        }
+        public IActionResult Branch(string id)
+        {
+            Branch br = new Branch();
+            br.Compalst = BindCompany();
+            br.cuntylst = BindCountry();
+            br.statlst = BindState();
+
+
+            return View(br);
+        }
+
+        public List<SelectListItem> BindCompany()
+        {
+            try
+            {
+                DataTable dtDesg = BranchService.GetCompany();
+                List<SelectListItem> lstdesg = new List<SelectListItem>();
+                for (int i = 0; i < dtDesg.Rows.Count; i++)
+                {
+                    lstdesg.Add(new SelectListItem() { Text = dtDesg.Rows[i]["COMPANYID"].ToString(), Value = dtDesg.Rows[i]["COMPANYMASTID"].ToString() });
+                }
+                return lstdesg;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        public List<SelectListItem> BindCountry()
+        {
+            try
+            {
+                DataTable dtDesg = BranchService.Getcountry();
+                List<SelectListItem> lstdesg = new List<SelectListItem>();
+                for (int i = 0; i < dtDesg.Rows.Count; i++)
+                {
+                    lstdesg.Add(new SelectListItem() { Text = dtDesg.Rows[i]["COUNTRYNAME"].ToString(), Value = dtDesg.Rows[i]["COUNTRYMASTID"].ToString() });
+                }
+                return lstdesg;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        public List<SelectListItem> BindState()
+        {
+            try
+            {
+                DataTable dtDesg = BranchService.GetState();
+                List<SelectListItem> lstdesg = new List<SelectListItem>();
+                for (int i = 0; i < dtDesg.Rows.Count; i++)
+                {
+                    lstdesg.Add(new SelectListItem() { Text = dtDesg.Rows[i]["STATE"].ToString(), Value = dtDesg.Rows[i]["STATEMASTID"].ToString() });
+                }
+                return lstdesg;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
         }
         public IActionResult ListBranch()
         {
             return View();
         }
+
     }
 }
+
+
+
+
--- Arasan/Models/Master/ItemSubGroup.cs
+++ Arasan/Models/Master/ItemSubGroup.cs
@@ -1,8 +1,13 @@
 ﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.SignalR;
 
 namespace Arasan.Models
 {
     public class ItemSubGroup
     {
+        public string ID { get; set; }
+
+        public string itemSubGroup { get; set; }
+        public string Descreption { get; set; }
     }
 }
