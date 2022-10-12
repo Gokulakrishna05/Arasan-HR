aa108953ea3fa4db911e6f8d2640eba0e5afc65d
Gokulakrishna05 <gokulakrishna76@gmail.com>
2022-10-11 18:19:27 -0700

    Completed

--- Arasan/Models/Master/Location.cs
+++ Arasan/Models/Master/Location.cs
@@ -1,22 +1,31 @@
 ﻿
 
+using Microsoft.AspNetCore.Mvc.Rendering;
+
 namespace Arasan.Models
 {
     public class Location
     {
         public string ID { get; set; }
         public string LocationId { get; set; }
-        public string LocationType { get; set; }
+        public string LocType { get; set; }
         public string ContactPer { get; set; }
         public string PhoneNo { get; set; }
-        public string FaxNo { get; set; }
+        //public string FaxNo { get; set; }
         public string EmailId { get; set; }
         public string Address { get; set; }
-        public string Branch { get; set; }
-        public string Bin { get; set; }
-        public string Trade { get; set; }
+      
+        //public string Bin { get; set; }
+        //public string Trade { get; set; }
   
-        public string FlowOrd { get; set; }
+        //public string FlowOrd { get; set; }
 
+        public Location()
+        {
+            this.Brlst = new List<SelectListItem>();
+
+        }
+        public List<SelectListItem> Brlst;
+        public string Branch { get; set; }
     }
 }
