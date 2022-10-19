0e16295cf2c09ec030679e4fdf4167e6b5aebd29
deepaicand <deepa@icand.in>
2022-10-19 16:09:37 +0530

    commit

--- Arasan/Controllers/Store Management/PurchaseIndentController.cs
+++ Arasan/Controllers/Store Management/PurchaseIndentController.cs
@@ -12,9 +12,13 @@ namespace Arasan.Controllers.Store_Management
     public class PurchaseIndentController : Controller
     {
         IPurchaseIndent PurIndent;
-        public PurchaseIndentController(IPurchaseIndent _PurService)
+        private string? _connectionString;
+        IConfiguration? _configuratio;
+        DataTransactions datatrans;
+        public PurchaseIndentController(IPurchaseIndent _PurService, IConfiguration _configuratio)
         {
             PurIndent = _PurService;
+            _connectionString = _configuratio.GetConnectionString("OracleDBConnection");
         }
         public IActionResult Purchase_Indent()
         {
@@ -179,7 +183,7 @@ namespace Arasan.Controllers.Store_Management
         {
             List<IndentItemBindList> EnqChkItem = new List<IndentItemBindList>();
             DataTable dtEnq = new DataTable();
-            dtEnq = PurIndent.GetIndentItemApprove();
+            dtEnq = PurIndent.GetIndentItemApproved();
             for (int i = 0; i < dtEnq.Rows.Count; i++)
             {
                 EnqChkItem.Add(new IndentItemBindList
@@ -202,6 +206,60 @@ namespace Arasan.Controllers.Store_Management
             });
         }
 
+        public ActionResult ListIndentItemgridSupp(string PRID)
+        {
+            List<IndentItemBindList> EnqChkItem = new List<IndentItemBindList>();
+            DataTable dtEnq = new DataTable();
+            dtEnq = PurIndent.GetIndentItemSupp();
+            string assign = string.Empty;
+            
+            for (int i = 0; i < dtEnq.Rows.Count; i++)
+            {
+                assign = "IndentSupAllocation?id=" + dtEnq.Rows[i]["ITEMMASTERID"].ToString() + "";
+                EnqChkItem.Add(new IndentItemBindList
+                {
+                    
+                    itemname = dtEnq.Rows[i]["ITEMID"].ToString(),
+                    itemid = dtEnq.Rows[i]["ITEMMASTERID"].ToString(),
+                    quantity = dtEnq.Rows[i]["QTY"].ToString(),
+                    assign= assign,
+                    
+                });
+            }
+
+            return Json(new
+            {
+                EnqChkItem
+            });
+        }
+
+        public ActionResult ListIndentItemgridSuppDetails()
+        {
+            List<IndentItemBindList> EnqChkItem = new List<IndentItemBindList>();
+            DataTable dtEnq = new DataTable();
+            dtEnq = PurIndent.GetIndentItemSuppDetail();
+            for (int i = 0; i < dtEnq.Rows.Count; i++)
+            {
+                EnqChkItem.Add(new IndentItemBindList
+                {
+                    indentid = Convert.ToInt64(dtEnq.Rows[i]["PINDDETAILID"].ToString()),
+                    piid = Convert.ToInt64(dtEnq.Rows[i]["PINDBASICID"].ToString()),
+                    itemid= dtEnq.Rows[i]["ITEMMASTERID"].ToString(),
+                    itemname = dtEnq.Rows[i]["ITEMID"].ToString(),
+                    unit = dtEnq.Rows[i]["UNITID"].ToString(),
+                    quantity = dtEnq.Rows[i]["QTY"].ToString(),
+                    location = dtEnq.Rows[i]["LOCID"].ToString(),
+                    duedate = dtEnq.Rows[i]["DUEDATE"].ToString(),
+                    indentno = dtEnq.Rows[i]["DOCID"].ToString(),
+                    indentdate = dtEnq.Rows[i]["DOCDATE"].ToString(),
+                });
+            }
+
+            return Json(new
+            {
+                EnqChkItem
+            });
+        }
         public List<SelectListItem> BindBranch()
         {
             try
@@ -292,15 +350,47 @@ namespace Arasan.Controllers.Store_Management
             return Json(BindItemlst(itemid));
 
         }
+        //public JsonResult GetSuppJSON(string partytype)
+        //{
+        //    IndentSuppAllocate model = new IndentSuppAllocate();
+        //    model.Partylst = BindItemlst(itemid);
+        //    return Json(BindItemlst(itemid));
+
+        //}
         public JsonResult GetItemGrpJSON()
         {
             //EnqItem model = new EnqItem();
             //model.ItemGrouplst = BindItemGrplst(value);
             return Json(BindItemGrplst());
         }
+        public JsonResult GetSupJSON()
+        {
+            return Json(BindSupplier());
+        }
+        public JsonResult GetexistingSupJSON(string itemid)
+        {
+            return Json(BindOldSupp(itemid));
+        }
+
+        public List<SelectListItem> BindSupplier()
+        {
+            try
+            {
+                DataTable dtDesg = PurIndent.GetSupplier();
+                List<SelectListItem> lstdesg = new List<SelectListItem>();
+                for (int i = 0; i < dtDesg.Rows.Count; i++)
+                {
+                    lstdesg.Add(new SelectListItem() { Text = dtDesg.Rows[i]["PARTY"].ToString(), Value = dtDesg.Rows[i]["PARTYMASTID"].ToString() });
+                }
+                return lstdesg;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
         public JsonResult GetLocGSON()
         {
-     
             return Json(GetLoc());
         }
 
@@ -340,6 +430,8 @@ namespace Arasan.Controllers.Store_Management
         }
         public ActionResult IndentApproved(string id)
         {
+            datatrans = new DataTransactions(_connectionString);
+            bool result = datatrans.UpdateStatus("UPDATE PINDDETAIL SET APPROVED1='YES',APPROVAL1U='SRRAJAN',APP1DT='" + DateTime.Now.ToString("dd-MMM-yyyy") + "' Where PINDDETAILID='" + id  + "'");
             return RedirectToAction("List_PI_Approval");
         }
             public List<SelectListItem> BindItemGrplst()
@@ -393,6 +485,43 @@ namespace Arasan.Controllers.Store_Management
                 throw ex;
             }
         }
+        public ActionResult GetSupp(string ItemId,string SuppId)
+        {
+            try
+            {
+                string rate = "";
+                string unit = "";
+                string lpprice = "";
+                string lpdate = "";
+                string qty = "";
+                DataTable ds = new DataTable();
+                ds = PurIndent.GetSuppPurchaseDetails(SuppId, ItemId);
+
+                if (ds.Rows.Count > 0)
+                {
+                    rate = ds.Rows[0]["RATE"].ToString();
+                    unit = ds.Rows[0]["PUNIT"].ToString();
+                    lpprice = ds.Rows[0]["TOTAMT"].ToString();
+                    lpdate = ds.Rows[0]["DOCDATE"].ToString();
+                    qty = ds.Rows[0]["QTY"].ToString();
+                }
+                else
+                {
+                    rate = "New";
+                    unit = "New";
+                    lpprice = "New";
+                    lpdate = "New";
+                    qty = "New";
+                }
+
+                var result = new { rate = rate, unit = unit, lpprice = lpprice , lpdate = lpdate , qty = qty };
+                return Json(result);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
         public IActionResult List_Purchase_Indent()
         {
             return View();
@@ -406,6 +535,85 @@ namespace Arasan.Controllers.Store_Management
         {
             return View();
         }
+        public IActionResult List_PI_Supp_Allocation()
+        {
+            return View();
+        }
+        public ActionResult IndentSupAllocation(string id)
+        {
+            IndentSupAllocation ca = new IndentSupAllocation();
+            List<IndentSupAllocationList> TData = new List<IndentSupAllocationList>();
+            IndentSupAllocationList tda = new IndentSupAllocationList();
+            DataTable dt = new DataTable();
+            dt = PurIndent.GetHistory(id);
+            DataTable da = new DataTable();
+            da = PurIndent.GetIndentItemSuppEnq(id);
+            if(da.Rows.Count > 0)
+            {
+                ca.ItemName= da.Rows[0]["ITEMID"].ToString();
+                ca.TotalQty = da.Rows[0]["QTY"].ToString();
+                ca.ItemId= da.Rows[0]["ITEMMASTERID"].ToString(); 
+            }
+            for (int i = 0; i < dt.Rows.Count; i++)
+            {
+                tda = new IndentSupAllocationList();
+                tda.PartyName = dt.Rows[i]["PARTY"].ToString();
+                tda.Rate = dt.Rows[i]["RATE"].ToString();
+                tda.Unit = dt.Rows[i]["PUNIT"].ToString();
+                ca.UnitP = tda.Unit;
+                tda.LastPurchsePrice = dt.Rows[i]["TOTAMT"].ToString();
+                tda.LastPurchaseDate = dt.Rows[i]["DOCDATE"].ToString();
+                tda.Qty = dt.Rows[i]["QTY"].ToString();
+                TData.Add(tda);
+            }
+            ca.HSupLst = TData;
 
+
+            List<IndentSuppAllocate> TDataa = new List<IndentSuppAllocate>();
+            IndentSuppAllocate tdaa = new IndentSuppAllocate();
+            DataTable dt1 = new DataTable();
+            dt1 = PurIndent.GetLasttwoSupp(id);
+            for (int i = 0; i < dt1.Rows.Count; i++)
+            {
+                tdaa= new IndentSuppAllocate();
+                tdaa.Partytype = "Existing";
+                tdaa.Partylst = BindOldSupp(id);
+                tdaa.PartyName = dt1.Rows[i]["PARTYID"].ToString();
+                DataTable ds = new DataTable();
+                ds = PurIndent.GetSuppPurchaseDetails(dt1.Rows[i]["PARTYID"].ToString(),id);
+                if(ds.Rows.Count > 0)
+                {
+                    tdaa.Rate = ds.Rows[0]["RATE"].ToString();
+                    tdaa.Unit = ds.Rows[0]["PUNIT"].ToString();
+                    tdaa.LastPurchsePrice = ds.Rows[0]["TOTAMT"].ToString();
+                    tdaa.LastPurchaseDate = ds.Rows[0]["DOCDATE"].ToString();
+                    tdaa.Qty = ds.Rows[0]["QTY"].ToString();
+                }
+                tdaa.EnquiryQty = ca.TotalQty;
+                tdaa.Isvalid = "Y";
+                TDataa.Add(tdaa);
+            }
+                ca.indentSuppAllocates = TDataa;
+            return View(ca);
+        }
+
+        public List<SelectListItem> BindOldSupp(string id)
+        {
+            try
+            {
+                DataTable dt1 = new DataTable();
+                dt1 = PurIndent.GetLasttwoSupp(id);
+                List<SelectListItem> lstdesg = new List<SelectListItem>();
+                for (int i = 0; i < dt1.Rows.Count; i++)
+                {
+                    lstdesg.Add(new SelectListItem() { Text = dt1.Rows[i]["PARTY"].ToString(), Value = dt1.Rows[i]["PARTYID"].ToString() });
+                }
+                return lstdesg;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
     }
 }
