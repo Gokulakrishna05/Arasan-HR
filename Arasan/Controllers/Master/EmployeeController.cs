21e04fa0e936494ecbf89f0f0c804ca606cbacc4
Sundara Moorthy <sundaramoorthy@icand.in>
2022-11-10 11:59:39 +0530

    develope

---
+++ Arasan/Controllers/Master/EmployeeController.cs
@@ -0,0 +1,14 @@
+﻿using Arasan.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Arasan.Controllers.Master
+{
+    public class EmployeeController : Controller
+    {
+        public IActionResult Employee()
+        {
+            Employee E = new Employee();
+            return View(E);
+        }
+    }
+}
---
+++ Arasan/Interface/Master/IEmployee.cs
@@ -0,0 +1,7 @@
+﻿namespace Arasan.Interface.Master
+{
+    public interface IEmployee
+    {
+
+    }
+}
