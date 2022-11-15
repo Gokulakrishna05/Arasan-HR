4cd82f83d949d109eb6735b80098004d38049d25
kesavamoorthi007 <kesavanmoorthi81@gmail.com>
2022-11-15 18:23:51 +0530

    complete

--- Arasan/Services/Master/ItemNameService.cs
+++ Arasan/Services/Master/ItemNameService.cs
@@ -192,6 +192,16 @@ namespace Arasan.Services.Master
             adapter.Fill(dtt);
             return dtt;
         }
+        public DataTable GetSupplier()
+        {
+            string SvSql = string.Empty;
+            SvSql = "Select PARTYMAST.PARTYMASTID,PARTYRCODE.PARTY from PARTYMAST LEFT OUTER JOIN PARTYRCODE ON PARTYMAST.PARTYID=PARTYRCODE.ID Where PARTYMAST.TYPE IN ('Supplier','BOTH') AND PARTYRCODE.PARTY IS NOT NULL";
+            DataTable dtt = new DataTable();
+            OracleDataAdapter adapter = new OracleDataAdapter(SvSql, _connectionString);
+            OracleCommandBuilder builder = new OracleCommandBuilder(adapter);
+            adapter.Fill(dtt);
+            return dtt;
+        }
 
     }
 
