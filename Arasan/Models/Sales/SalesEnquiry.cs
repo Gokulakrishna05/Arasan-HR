c2d299a179f75d7b267eea3fe78f416bd0c2c628
Gokulakrishna05 <gokulakrishna76@gmail.com>
2023-02-14 18:15:05 +0530

    Complete

--- Arasan/Models/Sales/SalesEnquiry.cs
+++ Arasan/Models/Sales/SalesEnquiry.cs
@@ -7,34 +7,41 @@ namespace Arasan.Models
         public SalesEnquiry()
     {
         this.Brlst = new List<SelectListItem>();
-        //this.Suplst = new List<SelectListItem>();
+        this.Suplst = new List<SelectListItem>();
         this.Curlst = new List<SelectListItem>();
-        //this.Loclst = new List<SelectListItem>();
-    }
+            this.RecList = new List<SelectListItem>();
+            this.assignList = new List<SelectListItem>();
+            this.Prilst = new List<SelectListItem>();
+            this.Enqlst = new List<SelectListItem>();
+        }
     public List<SelectListItem> Brlst;
     public string ID { get; set; }
 
     public string Branch { get; set; }
-
-   // public string Supplier { get; set; }
+        public string EnqDate { get; set; }
+        public string Customer { get; set; }
 
     public List<SelectListItem> Suplst;
 
     public string Currency { get; set; }
 
     public List<SelectListItem> Curlst;
-   // public List<SelectListItem> Loclst;
-    public string Location { get; set; }
-    public string ReqDate { get; set; }
-    public string RetDate { get; set; }
-    public double Packingcharges { get; set; }
-    public double Frieghtcharge { get; set; }
-    public double Othercharges { get; set; }
-    public double Round { get; set; }
-    public double otherdeduction { get; set; }
-    public double Roundminus { get; set; }
-    public double Gross { get; set; }
-    public double Net { get; set; }
-}
+        public List<SelectListItem> RecList;
+        public string Assign { get; set; }
+        public List<SelectListItem> assignList;
+        public string Recieved { get; set; }
+        public List<SelectListItem> Prilst;
+        public string Mobile { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+        public List<SelectListItem> Enqlst;
+        public string CustomerType { get; set; }
+        public string EnqType { get; set; }
+        public string City { get; set; }
+        public string PinCode { get; set; }
+        public string EnqNo { get; set; }
+        public string ContactPersion { get; set; }
+        public string Priority { get; set; }
+    }
 }
    
--- Arasan/Services/Sales/SalesEnqService.cs
+++ Arasan/Services/Sales/SalesEnqService.cs
@@ -4,56 +4,147 @@ using Microsoft.Extensions.Configuration;
 using Oracle.ManagedDataAccess.Client;
 using System;
 using System.Collections.Generic;
+using System.Data;
+
 namespace Arasan.Services
 {
     public class SalesEnqService : ISalesEnq
     {
-        //public class StockInService : IStockIn
-        //{
-        //    private readonly string _connectionString;
-        //    public StockInService(IConfiguration _configuratio)
-        //    {
-        //        _connectionString = _configuratio.GetConnectionString("OracleDBConnection");
-        //    }
-        //    public IEnumerable<StockIn> GetAllStock()
-        //    {
-        //        List<StockIn> cmpList = new List<StockIn>();
-        //        using (OracleConnection con = new OracleConnection(_connectionString))
-        //        {
+        private readonly string _connectionString;
+        DataTransactions datatrans;
+        public SalesEnqService(IConfiguration _configuratio)
+        {
+            _connectionString = _configuratio.GetConnectionString("OracleDBConnection");
+        }
+        public IEnumerable<SalesEnquiry> GetAllSalesEnq()
+        {
+            List<SalesEnquiry> cmpList = new List<SalesEnquiry>();
+            using (OracleConnection con = new OracleConnection(_connectionString))
+            {
 
-        //            using (OracleCommand cmd = con.CreateCommand())
-        //            {
-        //                con.Open();
-        //                cmd.CommandText = "Select SUM(INVENTORY_ITEM.BALANCE_QTY) as QTY,ITEMMASTER.ITEMID,UNITMAST.UNITID,BRANCHMAST.BRANCHID,LOCDETAILS.LOCID from INVENTORY_ITEM LEFT OUTER JOIN ITEMMASTER on ITEMMASTER.ITEMMASTERID=INVENTORY_ITEM.ITEM_ID LEFT OUTER JOIN UNITMAST ON UNITMAST.UNITMASTID=ITEMMASTER.PRIUNIT LEFT OUTER JOIN BRANCHMAST ON BRANCHMASTID=INVENTORY_ITEM.BRANCH_ID LEFT OUTER JOIN LOCDETAILS ON LOCDETAILS.LOCDETAILSID=INVENTORY_ITEM.LOCATION_ID GROUP BY ITEMMASTER.ITEMID,UNITMAST.UNITID,BRANCHMAST.BRANCHID,LOCDETAILS.LOCID ";
-        //                OracleDataReader rdr = cmd.ExecuteReader();
-        //                while (rdr.Read())
-        //                {
-        //                    StockIn cmp = new StockIn
-        //                    {
+                using (OracleCommand cmd = con.CreateCommand())
+                {
+                    con.Open();
+                    cmd.CommandText = "Select  PARTYRCODE.PARTY,ENQ_NO,to_char(ENQ_DATE,'dd-MON-yyyy')ENQ_DATE, ENQ_TYPE,CUSTOMER_TYPE,CUSTOMER_NAME,SALES_ENQUIRY.ID from SALES_ENQUIRY LEFT OUTER JOIN  PARTYMAST on SALES_ENQUIRY.CUSTOMER_NAME=PARTYMAST.PARTYMASTID  LEFT OUTER JOIN PARTYRCODE ON PARTYMAST.PARTYID=PARTYRCODE.ID Where PARTYMAST.TYPE IN ('Customer','BOTH')\r\n";
+                    OracleDataReader rdr = cmd.ExecuteReader();
+                    while (rdr.Read())
+                    {
+                        SalesEnquiry cmp = new SalesEnquiry
+                        {
 
-        //                        //ID = rdr["DPBASICID"].ToString(),
-        //                        Branch = rdr["BRANCHID"].ToString(),
-        //                        Item = rdr["ITEMID"].ToString(),
-        //                        Unit = rdr["UNITID"].ToString(),
-        //                        //GoodQty = rdr["DOCDATE"].ToString(),
-        //                        Qty = rdr["QTY"].ToString(),
-
-        //                        Location = rdr["LOCID"].ToString(),
-        //                        //Currency = rdr["MAINCURR"].ToString(),
-        //                        //Narration = rdr["NARR"].ToString()
+                            ID = rdr["ID"].ToString(),
+                            Branch = rdr["BRANCHID"].ToString(),
+                            Customer = rdr["PARTY"].ToString(),
+                            EnqNo = rdr["ENQ_NO"].ToString(),
+                            EnqDate = rdr["ENQ_DATE"].ToString(),
+                            CustomerType = rdr["CUSTOMER_TYPE"].ToString(),
+                            EnqType = rdr["ENQ_TYPE"].ToString(),
+                         
+                            
 
 
 
+                        };
+                        cmpList.Add(cmp);
+                    }
+                }
+            }
+            return cmpList;
+        }
+        public string SalesEnqCRUD(SalesEnquiry cy)
+        {
+            string msg = "";
+            try
+            {
+                string StatementType = string.Empty; string svSQL = "";
 
-        //                    };
-        //                    cmpList.Add(cmp);
-        //                }
-        //            }
-        //        }
-        //        return cmpList;
-        //    }
+                using (OracleConnection objConn = new OracleConnection(_connectionString))
+                {
+                    OracleCommand objCmd = new OracleCommand("SALESENQPROC", objConn);
+                    /*objCmd.Connection = objConn;
+                    objCmd.CommandText = "DIRECTPURCHASEPROC";*/
 
-        //}
+                    objCmd.CommandType = CommandType.StoredProcedure;
+                    if (cy.ID == null)
+                    {
+                        StatementType = "Insert";
+                        objCmd.Parameters.Add("ID", OracleDbType.NVarchar2).Value = DBNull.Value;
+                    }
+                    else
+                    {
+                        StatementType = "Update";
+                        objCmd.Parameters.Add("ID", OracleDbType.NVarchar2).Value = cy.ID;
+
+                    }
+                    objCmd.Parameters.Add("BRANCH_ID", OracleDbType.NVarchar2).Value = cy.Branch;
+                    objCmd.Parameters.Add("CUSTOMER_NAME", OracleDbType.NVarchar2).Value = cy.Customer;
+                    objCmd.Parameters.Add("ENQ_NO", OracleDbType.NVarchar2).Value = cy.EnqNo;
+                    objCmd.Parameters.Add("ENQ_DATE", OracleDbType.Date).Value = DateTime.Parse(cy.EnqDate);
+                    objCmd.Parameters.Add("CITY", OracleDbType.NVarchar2).Value = cy.City;
+              
+                    objCmd.Parameters.Add("CUSTOMER_TYPE", OracleDbType.NVarchar2).Value = cy.CustomerType;
+                    objCmd.Parameters.Add("ENQ_TYPE", OracleDbType.NVarchar2).Value = cy.EnqType;
+                    objCmd.Parameters.Add("CURRENCY_TYPE", OracleDbType.NVarchar2).Value = cy.Currency;
+                    objCmd.Parameters.Add("ADDRESS", OracleDbType.NVarchar2).Value = cy.Address;
+                    objCmd.Parameters.Add("CONTACT_PERSON", OracleDbType.NVarchar2).Value = cy.ContactPersion;
+                    objCmd.Parameters.Add("CONTACT_PERSON_MOBILE", OracleDbType.NVarchar2).Value = cy.Mobile;
+                    objCmd.Parameters.Add("PRIORITY", OracleDbType.NVarchar2).Value = cy.Priority;
+                    objCmd.Parameters.Add("LEADBY", OracleDbType.NVarchar2).Value = cy.Recieved;
+                    objCmd.Parameters.Add("ASSIGNED_TO", OracleDbType.NVarchar2).Value = cy.Assign;
+                    objCmd.Parameters.Add("PINCODE", OracleDbType.NVarchar2).Value = cy.PinCode;
+                    //objCmd.Parameters.Add("NARR", OracleDbType.NVarchar2).Value = cy.Narration;
+                    objCmd.Parameters.Add("StatementType", OracleDbType.NVarchar2).Value = StatementType;
+                    objCmd.Parameters.Add("OUTID", OracleDbType.Int64).Direction = ParameterDirection.Output;
+                    try
+                    {
+                        objConn.Open();
+                        objCmd.ExecuteNonQuery();
+
+
+                        objConn.Close();
+                              
+
+
+                            }
+                       
+                      
+              
+                    catch (Exception ex)
+                    {
+                        //System.Console.WriteLine("Exception: {0}", ex.ToString());
+                    }
+                    objConn.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                msg = "Error Occurs, While inserting / updating Data";
+                throw ex;
+            }
+
+            return msg;
+        }
+        public DataTable GetSalesEnquiry(string id)
+        {
+            string SvSql = string.Empty;
+            SvSql = "Select  BRANCH_ID,ENQ_NO,to_char(ENQ_DATE,'dd-MON-yyyy')ENQ_DATE, ENQ_TYPE,CUSTOMER_TYPE,CUSTOMER_NAME,ADDRESS,CITY,PINCODE,CONTACT_PERSON,CONTACT_PERSON_MOBILE,PRIORITY,LEADBY,ASSIGNED_TO,CURRENCY_TYPE,SALES_ENQUIRY.ID from SALES_ENQUIRY  where SALES_ENQUIRY.ID=" + id + "";
+            DataTable dtt = new DataTable();
+            OracleDataAdapter adapter = new OracleDataAdapter(SvSql, _connectionString);
+            OracleCommandBuilder builder = new OracleCommandBuilder(adapter);
+            adapter.Fill(dtt);
+            return dtt;
+        }
+
+        public DataTable GetSupplier()
+        {
+            string SvSql = string.Empty;
+            SvSql = "Select PARTYMAST.PARTYMASTID,PARTYRCODE.PARTY from PARTYMAST LEFT OUTER JOIN PARTYRCODE ON PARTYMAST.PARTYID=PARTYRCODE.ID Where PARTYMAST.TYPE IN ('Customer','BOTH') AND PARTYRCODE.PARTY IS NOT NULL";
+            DataTable dtt = new DataTable(); OracleDataAdapter adapter = new OracleDataAdapter(SvSql, _connectionString);
+
+            OracleCommandBuilder builder = new OracleCommandBuilder(adapter);
+            adapter.Fill(dtt);
+            return dtt;
+        }
     }
 }
 
