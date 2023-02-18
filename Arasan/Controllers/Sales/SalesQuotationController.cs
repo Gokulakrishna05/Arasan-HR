b7ed3c7e787f42905087f4685d561ca7196c2747
kesavamoorthi007 <kesavamoorthi81@gmail.com>
2023-02-18 10:36:07 +0530

    Complete

--- Arasan/Controllers/Sales/SalesQuotationController.cs
+++ Arasan/Controllers/Sales/SalesQuotationController.cs
@@ -3,6 +3,7 @@ using Arasan.Models;
 using Arasan.Interface.Sales;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using System.Data;
+using System.Collections.Generic;
 
 namespace Arasan.Controllers.Sales
 {
@@ -25,8 +26,11 @@ namespace Arasan.Controllers.Sales
             ca.Brlst = BindBranch();
             ca.assignList = BindEmp();
             ca.Curlst = BindCurrency();
+            ca.Prilst = BindPriority();
             ca.Categorylst = BindCategory();
             ca.cuntylst = BindCountry();
+            ca.Enqlst = BindEnqType();
+            ca.Typelst = BindCusType();
             List<QuoItem> Data = new List<QuoItem>();
             QuoItem tda = new QuoItem();
             if (id == null)
@@ -40,17 +44,18 @@ namespace Arasan.Controllers.Sales
                 dt = SalesQuotationService.GetSalesQuotation(id);
                 if (dt.Rows.Count > 0)
                 {
-                    //ca.Branch = dt.Rows[0]["BRANCHID"].ToString();
-                    //ca.DocDate = dt.Rows[0]["DOCDATE"].ToString();
-                    //ca.Supplier = dt.Rows[0]["PARTYID"].ToString();
-                    //ca.EnqNo = dt.Rows[0]["ENQNO"].ToString();
-                    //ca.EnqDate = dt.Rows[0]["ENQDATE"].ToString();
-                    //ca.ExRate = dt.Rows[0]["EXRATE"].ToString();
-                    //ca.ID = id;
-                    ////ca.ParNo = dt.Rows[0]["PARTYREFNO"].ToString();
-                    //ca.Currency = dt.Rows[0]["MAINCURRENCY"].ToString();
-                    ////ca.ExRate = dt.Rows[0]["EXCRATERATE"].ToString();
-                    //ca.QuoId = dt.Rows[0]["DOCID"].ToString();
+                    ca.Branch = dt.Rows[0]["BRANCHID"].ToString();
+                    ca.QuoId = dt.Rows[0]["QUOTE_NO"].ToString();
+                    ca.QuoDate = dt.Rows[0]["QUOTE_DATE"].ToString();
+                    ca.EnNo = dt.Rows[0]["ENQNO"].ToString();
+                    ca.EnDate = dt.Rows[0]["ENQDATE"].ToString();
+                    ca.Currency = dt.Rows[0]["CURRENCY_TYPE"].ToString();
+                    ca.Customer = dt.Rows[0]["CUSTOMER"].ToString();
+                    ca.Address = dt.Rows[0]["ADDRESS"].ToString();
+                    ca.City = dt.Rows[0]["CITY"].ToString();
+                    ca.ID = id;
+                    
+                    
                 }
                 //ca =SalesQuotationService.GetLocationsById(id);
                 DataTable dt2 = new DataTable();
@@ -136,6 +141,80 @@ namespace Arasan.Controllers.Sales
         {
             IEnumerable<SalesQuotation> cmp = SalesQuotationService.GetAllSalesQuotation();
             return View(cmp);
+        }
+        public List<SelectListItem> BindCusType()
+        {
+            try
+            {
+                DataTable dtDesg = SalesQuotationService.GetCusType();
+                List<SelectListItem> lstdesg = new List<SelectListItem>();
+                for (int i = 0; i < dtDesg.Rows.Count; i++)
+                {
+                    lstdesg.Add(new SelectListItem() { Text = dtDesg.Rows[i]["CUSTOMER_TYPE"].ToString(), Value = dtDesg.Rows[i]["ID"].ToString() });
+                }
+                return lstdesg;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        public List<SelectListItem> BindEnqType()
+        {
+            try
+            {
+                List<SelectListItem> lstdesg = new List<SelectListItem>();
+                lstdesg.Add(new SelectListItem() { Text = "Phone", Value = "Phone" });
+                lstdesg.Add(new SelectListItem() { Text = "Email", Value = "Email" });
+                lstdesg.Add(new SelectListItem() { Text = "Personal Visit", Value = "Personal Visit" });
+                lstdesg.Add(new SelectListItem() { Text = "Tender", Value = "Tender" });
+                lstdesg.Add(new SelectListItem() { Text = "Lead from Principals", Value = "Lead from Principals" });
+                lstdesg.Add(new SelectListItem() { Text = "Expo Lead", Value = "Expo Lead" });
+                lstdesg.Add(new SelectListItem() { Text = "IndiaMart", Value = "IndiaMart " });
+                lstdesg.Add(new SelectListItem() { Text = "Lab Expo", Value = "Lab Expo" });
+                lstdesg.Add(new SelectListItem() { Text = "Others", Value = "Others" });
+
+
+                return lstdesg;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        public ActionResult GetCustomerDetail(string ItemId)
+        {
+            try
+            {
+                DataTable dt = new DataTable();
+                DataTable dt1 = new DataTable();
+
+                string address = "";
+
+                string city = "";
+                string pin = "";
+                dt = SalesQuotationService.GetCustomerDetails(ItemId);
+
+                if (dt.Rows.Count > 0)
+                {
+
+                    address = dt.Rows[0]["ADD1"].ToString();
+
+
+                    city = dt.Rows[0]["CITY"].ToString();
+
+
+                    pin = dt.Rows[0]["PINCODE"].ToString();
+
+                }
+
+                var result = new { address = address, city = city, pin = pin };
+                return Json(result);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
         }
         public ActionResult GetItemDetail(string ItemId)
         {
@@ -254,6 +333,22 @@ namespace Arasan.Controllers.Sales
                 throw ex;
             }
         }
+        public List<SelectListItem> BindPriority()
+        {
+            try
+            {
+                List<SelectListItem> lstdesg = new List<SelectListItem>();
+                lstdesg.Add(new SelectListItem() { Text = "Normal", Value = "Normal" });
+                lstdesg.Add(new SelectListItem() { Text = "Urgent", Value = "Urgent" });
+                lstdesg.Add(new SelectListItem() { Text = "Critical", Value = "Critical" });
+
+                return lstdesg;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
         public List<SelectListItem> BindCountry()
         {
             try
@@ -270,6 +365,67 @@ namespace Arasan.Controllers.Sales
             {
                 throw ex;
             }
+        }
+        public ActionResult SendMail(string id)
+        {
+            try
+            {
+                datatrans = new DataTransactions(_connectionString);
+                MailRequest requestwer = new MailRequest();
+
+                requestwer.ToEmail = "deepa@icand.in";
+                requestwer.Subject = "Quotations";
+                string Content = "";
+                IEnumerable<QuoItem> cmp = SalesQuotationService.GetAllSalesQuotationItem(id);
+                Content = @"<html> 
+                < head >
+    < style >
+                table, th, td {
+                border: 1px solid black;
+                    border - collapse: collapse;
+                }
+    </ style >
+</ head >
+< body >
+<p>Dear Sir,</p>
+</br>
+  <p> Kindly arrange to send your lowest price offer for the following items through our email immediately.</p>
+</br>
+< table style = 'border: 1px solid black;border-collapse: collapse;' > ";
+
+
+
+                foreach (QuoItem item in cmp)
+                {
+                    Content += " <tr><td>" + item.Desc + "</td>";
+                    Content += " <td>" + item.Quantity + "</td>";
+                    Content += " <td>" + item.Unit + "</td></tr>";
+                }
+                Content += "</table>";
+
+                Content += @" </br> 
+<p style='padding-left:30px;font-style:italic;'>With Regards,
+</br><img src='../assets/images/Arasan_Logo.png' alt='Arasan Logo'/>
+</br>N Balaji Purchase Manager
+</br>The Arasan Aluminium Industries (P) Ltd.
+<br/102-A
+
+</br>
+</p> ";
+                Content += @" </body> 
+</html> ";
+
+                requestwer.Body = Content;
+                //request.Attachments = "Yes";
+                datatrans.sendemail("Test mail", Content, "kesavanmoorthi81@gmail.com", "kesavanmoorthi70@gmail.com", "aqhfevhczfrnbtgz", "587", "true", "smtp.gmail.com", "Arasan");
+                return Ok();
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
         }
         public List<SelectListItem> BindCategory()
         {
