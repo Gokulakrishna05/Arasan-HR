7e97dd9da868bc44042f9f48dc1ac982474664ca
kesavamoorthi007 <kesavamoorthi81@gmail.com>
2023-03-27 10:28:22 +0530

    Complete

---
+++ Arasan/Interface/Qualitycontrol/IQCFinalValueEntryService.cs
@@ -0,0 +1,15 @@
+﻿using Arasan.Models;
+using System.Collections.Generic;
+using System.Collections;
+using Arasan.Models;
+using System.Data;
+
+namespace Arasan.Interface.Qualitycontrol
+{
+    public interface IQCFinalValueEntryService
+    {
+        DataTable GetWorkCenter();
+        DataTable GetProcess();
+        string QCFinalValueEntryCRUD(QCFinalValueEntry cy);
+    }
+}
---
+++ Arasan/Models/Qualitycontrol/QCFinalValueEntry.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Arasan.Models
+{
+    public class QCFinalValueEntry
+    {
+        public QCFinalValueEntry()
+        {
+
+            this.Brlst = new List<SelectListItem>();
+            this.Worklst = new List<SelectListItem>();
+            this.Processlst = new List<SelectListItem>();
+            this.RecList = new List<SelectListItem>();
+        }
+        public string ID { get; set; }
+        public List<SelectListItem> Brlst;
+        public string Branch { get; set; }
+
+        public List<SelectListItem> Worklst;
+        public string WorkCenter { get; set; }
+
+        public List<SelectListItem> Processlst;
+        public string Process { get; set; }
+
+        public List<SelectListItem> RecList;
+        public string Enterd { get; set; }
+    }
+}
