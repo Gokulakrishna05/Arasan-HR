891bc33240823766461dae874ec6293d88615fc2
kesavamoorthi007 <kesavanmoorthi70@gmail.com>
2023-05-03 11:56:33 +0530

    Complete

--- Arasan/Services/Master/CuringService.cs
+++ Arasan/Services/Master/CuringService.cs
@@ -44,7 +44,7 @@ namespace Arasan.Services
                 using (OracleCommand cmd = con.CreateCommand())
                 {
                     con.Open();
-                    cmd.CommandText = "Select LOCDETAILS.LOCID,SUBGROUP,SHEDNUMBER,NOFDAYS,STATUS,CURINGMASTERID  from CURINGMASTER LEFT OUTER JOIN LOCDETAILS ON LOCDETAILSID = CURINGMASTER.LOCATIONID";
+                    cmd.CommandText = "Select LOCDETAILS.LOCID,SUBGROUP,SHEDNUMBER,NOFDAYS,CURINGMASTERID  from CURINGMASTER LEFT OUTER JOIN LOCDETAILS ON LOCDETAILSID = CURINGMASTER.LOCATIONID";
                     OracleDataReader rdr = cmd.ExecuteReader();
                     while (rdr.Read())
                     {
@@ -55,7 +55,7 @@ namespace Arasan.Services
                             Sub = rdr["SUBGROUP"].ToString(),
                             Shed = rdr["SHEDNUMBER"].ToString(),
                             Days = rdr["NOFDAYS"].ToString(),
-                            Status = rdr["STATUS"].ToString()
+                            //Status = rdr["STATUS"].ToString()
                         };
                         cmpList.Add(cmp);
                     }
@@ -71,7 +71,7 @@ namespace Arasan.Services
                 using (OracleCommand cmd = con.CreateCommand())
                 {
                     con.Open();
-                    cmd.CommandText = "Select LOCATIONID,SUBGROUP,SHEDNUMBER,NOFDAYS,STATUS,CURINGMASTERID from CURINGMASTER where CURINGMASTERID=" + eid + "";
+                    cmd.CommandText = "Select LOCATIONID,SUBGROUP,SHEDNUMBER,NOFDAYS,CURINGMASTERID from CURINGMASTER where CURINGMASTERID=" + eid + "";
                     OracleDataReader rdr = cmd.ExecuteReader();
                     while (rdr.Read())
                     {
@@ -81,8 +81,8 @@ namespace Arasan.Services
                             Location = rdr["LOCATIONID"].ToString(),
                             Sub = rdr["SUBGROUP"].ToString(),
                             Shed = rdr["SHEDNUMBER"].ToString(),
-                            Days = rdr["NOFDAYS"].ToString(),
-                            Status = rdr["STATUS"].ToString()
+                            Days = rdr["NOFDAYS"].ToString()
+                            //Status = rdr["STATUS"].ToString()
                         };
                         Curing = cmp;
                     }
@@ -121,7 +121,7 @@ namespace Arasan.Services
                     objCmd.Parameters.Add("SUBGROUP", OracleDbType.NVarchar2).Value = cy.Sub;
                     objCmd.Parameters.Add("SHEDNUMBER", OracleDbType.NVarchar2).Value = cy.Shed;
                     objCmd.Parameters.Add("NOFDAYS", OracleDbType.NVarchar2).Value = cy.Days;
-                    objCmd.Parameters.Add("STATUS", OracleDbType.NVarchar2).Value = cy.Status;
+                    //objCmd.Parameters.Add("STATUS", OracleDbType.NVarchar2).Value = cy.Status;
                     objCmd.Parameters.Add("StatementType", OracleDbType.NVarchar2).Value = StatementType;
                     try
                     {
