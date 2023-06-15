51562780f1448560d460f2ef725ef6f2f9bd561e
kesavamoorthi007 <kesavanmoorthi70@gmail.com>
2023-06-15 18:12:30 +0530

    Complete

--- Arasan/Interface/Sales/IDepotInvoiceService.cs
+++ Arasan/Interface/Sales/IDepotInvoiceService.cs
@@ -7,5 +7,11 @@ namespace Arasan.Interface.Sales
     public interface IDepotInvoiceService
     {
         string DirectPurCRUD(DepotInvoice cy);
+        IEnumerable<DepotInvoice> GetAllDepotInvoice();
+        DataTable GetDepotInvoiceDeatils(string id);
+        DataTable GetEditItemDetails(string id);
+        string StatusChange(string tag, int id);
+        //DataTable GetDepotInvoiceItemDetails(string id);
+        DataTable GetItemCF(string ItemId, string unitid);
     }
 }
--- Arasan/Models/Sales/DepotInvoice.cs
+++ Arasan/Models/Sales/DepotInvoice.cs
@@ -21,6 +21,23 @@ namespace Arasan.Models
           
         }
         public string ID { get; set; }
+        public string InvNo { get; set; }
+        public string InvDate { get; set; }
+        public string RefNo { get; set; }
+        public string RefDate { get; set; }
+        public string ExRate { get; set; }
+        public string Cancel { get; set; }
+       
+        public string Customer { get; set; }
+        public string Sales { get; set; }
+        public string RecBy { get; set; }
+        public double Packing { get; set; }
+        public double Round { get; set; }
+        public double Gross { get; set; }
+        public double Net { get; set; }
+        public string AinWords { get; set; }
+        public string Serial { get; set; }
+        public string Narration { get; set; }
 
         public List<SelectListItem> Brlst;
         public string Branch { get; set; }
@@ -41,7 +58,7 @@ namespace Arasan.Models
         public string Type { get; set; }
 
         public List<SelectListItem> Orderlst;
-        public string Order { get; set; }
+        public string Ordsam { get; set; }
 
         public List<SelectListItem> Dislst;
         public string Dis { get; set; }
@@ -54,5 +71,42 @@ namespace Arasan.Models
 
         public List<SelectListItem> Voclst;
         public string Vocher { get; set; }
+
+        public List<DepotInvoiceItem> Depotlst { get; set; }
+    }
+    public class DepotInvoiceItem
+    {
+        public string ID { get; set; }
+        public string ItemId { get; set; }
+        public string saveItemId { get; set; }
+        public List<SelectListItem> Itemlst { get; set; }
+
+        public List<SelectListItem> ItemGrouplst { get; set; }
+
+        public string ItemGroupId { get; set; }
+
+        public double FrigCharge { get; set; }
+        public string ConFac { get; set; }
+        public string Unit { get; set; }
+        public string Isvalid { get; set; }
+        public double Quantity { get; set; }
+        public double rate { get; set; }
+        public double Amount { get; set; }
+
+        public double Disc { get; set; }
+        public double DiscAmount { get; set; }
+
+        public double CGSTP { get; set; }
+        public double SGSTP { get; set; }
+        public double IGSTP { get; set; }
+        public double CGST { get; set; }
+        public double SGST { get; set; }
+        public double IGST { get; set; }
+        public double IntroDiscount { get; set; }
+        public double CashDiscount { get; set; }
+        public double TradeDiscount { get; set; }
+        public double AddDiscount { get; set; }
+        public double SpecDiscount { get; set; }
+        public double TotalAmount { get; set; }
     }
 }
