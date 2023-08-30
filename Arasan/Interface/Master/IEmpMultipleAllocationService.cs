cc45e62dbdbe3db02145480564b5315c90f436b1
kesavamoorthi007 <kesavanmoorthi81@gmail.com>
2023-08-30 10:28:10 +0530

    Complete

---
+++ Arasan/Interface/Master/IEmpMultipleAllocationService.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Data;
+using Arasan.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Arasan.Interface.Master
+{
+    public interface IEmpMultipleAllocationService
+    {
+        string EmpMultipleAllocationCRUD(EmpMultipleAllocation cy);
+        //IEnumerable<EmpMultipleAllocation> GetAllEmpMultipleAllocation();
+        DataTable GetEmp();
+        DataTable GetEmpAllocation();
+        DataTable GetEmpMultipleItem(string PRID);
+        DataTable GetMlocation();
+    }
+}
---
+++ Arasan/Models/Master/EmpMultipleAllocation.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Arasan.Models
+{
+    public class EmpMultipleAllocation
+    {
+        public string ID { get; set; }
+
+        public EmpMultipleAllocation()
+        {
+            this.Emolst = new List<SelectListItem>();
+            this.Loclst = new List<SelectListItem>();
+        }
+
+        public string EDate { get; set; }
+        public List<SelectListItem> Emolst;
+        public string Emp { get; set; }
+        public List<SelectListItem> Loclst;
+        public string[] Location { get; set; }
+    }
+    public class EmpBindList
+    {
+        public long piid { get; set; }
+        public string emp { get; set; }
+        public string edate { get; set; }
+        public string EditRow { get; set; }
+        public string DelRow { get; set; }
+    }
+    public class  EmpMultipleItemBindList
+    {
+        public long piid { get; set; }
+
+        public string location { get; set; }
+    }
+}
