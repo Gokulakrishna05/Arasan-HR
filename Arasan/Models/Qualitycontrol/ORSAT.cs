a48e9aa6c009971ac60c37aebeccf8c4748b0abf
kesavamoorthi007 <kesavanmoorthi81@gmail.com>
2023-10-24 17:53:51 +0530

    Completed

---
+++ Arasan/Models/Qualitycontrol/ORSAT.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Data;
+
+namespace Arasan.Models
+{
+    public class ORSAT
+    {
+        public ORSAT()
+        {
+            this.Brlst = new List<SelectListItem>();
+            this.Doclst = new List<SelectListItem>();
+            
+        }
+
+        public List<SelectListItem> Brlst;
+        public string Branch { get; set; }
+
+        public List<SelectListItem> Doclst;
+        public string docid { get; set; }
+        public string ID { get; set; }
+        public string docdate { get; set; }
+        public string shift { get; set; }
+        public string work { get; set; }
+        public string entry { get; set; }
+        public string time { get; set; }
+        public string remarks { get; set; }
+
+        public List<ORSATdetails> OBSATlst { get; set; }
+    }
+
+    public class ORSATdetails
+    {
+        public string para { get; set; }
+        public string value { get; set; }
+        public string Isvalid { get; set; }
+
+    }
+}
