2d3dccff53165373659bb0053567949a64197446
kesavamoorthi007 <kesavanmoorthi70@gmail.com>
2024-01-09 13:55:24 +0530

    Complete

---
+++ Arasan/Interface/Master/IRateCodeService.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Data;
+using Arasan.Models;
+using Microsoft.AspNetCore.Mvc;
+namespace Arasan.Interface.Master
+{
+    public interface IRateCodeService
+    {
+        DataTable GetAllListRateCodeItem(string strStatus);
+        DataTable GetEditRateCode(string id);
+        string RateCodeCRUD(RateCode cy);
+        string StatusChange(string tag, string id);
+    }
+}
