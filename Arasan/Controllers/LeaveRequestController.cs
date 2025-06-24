daba11cdc95f7476bd1fb0f8cbe3168b5c4051c2
devteam845 <devteam@icand.in>
2025-06-24 19:14:36 +0530

    Leave Request View

---
+++ Arasan/Controllers/LeaveRequestController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Arasan.Controllers
+{
+    public class LeaveRequestController : Controller
+    {
+        public IActionResult LeaveRequest()
+        {
+            return View();
+        }
+
+        public IActionResult ListLeaveRequest()
+        {
+            return View();
+        }
+    }
+}
---
+++ Arasan/Services/LeaveRequestService.cs
@@ -0,0 +1,6 @@
+﻿namespace Arasan.Services
+{
+    public class LeaveRequestService
+    {
+    }
+}
